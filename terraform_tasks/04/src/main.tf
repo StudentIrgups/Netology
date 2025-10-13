3506c9eecfd82332b7bf559665e46a2fd13bb114
StudentIrgups <beznosov.1992@gmail.com>
2025-10-13 21:23:51 +0900

    04 continue

--- terraform_tasks/04/src/main.tf
+++ terraform_tasks/04/src/main.tf
@@ -1,19 +1,18 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+module "vpc_dev" {
+  source       = "./vpc_dev"
+  vpc_name     = var.vpc_name
+  cloud_id     = var.cloud_id
+  default_cidr = var.default_cidr
+  folder_id    = var.folder_id
+  default_zone = var.default_zone
 }
 
 module "marketing-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "stage" 
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  subnet_ids     = [module.vpc_dev.subnet_id]
   instance_name  = "webs"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
@@ -32,9 +31,9 @@ module "marketing-vm" {
 module "analytics-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "stage"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  subnet_ids     = [module.vpc_dev.subnet_id]
   instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
---
+++ terraform_tasks/04/src/vpc_dev/variables.tf
@@ -0,0 +1,27 @@
+###cloud vars
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "VPC network&subnet name"
+}
+
+
+
